1c8e51cf038edb751f586cb6ee1aa22f9d2359fa
Matthew Mabrey <mattmabrey1@gmail.com>
2021-04-07 18:25:08 -0400

    Campus Resources Access Level Change

--- GreenTCNJ-AdminPortal/js/admin-page-functions.js
+++ GreenTCNJ-AdminPortal/js/admin-page-functions.js
@@ -34,7 +34,7 @@ function generateSidebar(){
 
     if(accessLevels != null){
 
-        if(accessLevels["events"] === 1 || accessLevels["materials"] === 1 || accessLevels["news"] === 1 || accessLevels["reports"] === 1){
+        if(accessLevels["events"] === 1 || accessLevels["materials"] === 1 || accessLevels["news"] === 1 || accessLevels["reports"] === 1 || accessLevels["resources"] === 1){
 
             $("#initiatives-tables-heading").removeClass("d-none");
     
@@ -52,6 +52,10 @@ function generateSidebar(){
         
             if(accessLevels["reports"] === 1){
                 $("#reports-link").removeClass("d-none");
+            }
+
+            if(accessLevels["resources"] === 1){
+                $("#resources-link").removeClass("d-none");
             }
         }
     
